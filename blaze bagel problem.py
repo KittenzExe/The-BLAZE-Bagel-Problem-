03e68ee9ea042c141490a8070611304e877c38e4
KittenzExe <67358250+KittenzExe@users.noreply.github.com>
2023-01-16 17:30:10 +1100

    amazing

---
+++ blaze bagel problem.py
@@ -0,0 +1,50 @@
+#here is the best problem
+import time
+number = input("What is your favorite number? ")
+number = int(number)
+if number <= 10 and number > 0:
+    print("we1l... thats a low number...")
+    time.sleep(1)
+    print("let me think now...")
+    time.sleep(1)
+    print("You would enjoy creme cheese on your bagel")
+
+elif number < 42 and number > 10:
+    print("we1l... thats an adverage number...")
+    time.sleep(1)
+    print("let me think now...")
+    time.sleep(1)
+    print("You would enjoy cream cheese with sliced cucumbers and sun-dried tomato on your bagel")
+
+elif number == 42:
+    print("we1l... thats a... good number ay")
+    time.sleep(1)
+    print("make sure to make extra bagels for your pleh friends!")
+    time.sleep(1)
+    print("(is it watching us... the god...  the developer... Blazé?)")
+
+elif number < 69 and number > 42:
+    print("we1l... thats an adverage number...")
+    time.sleep(1)
+    print("let me think now...")
+    time.sleep(1)
+    print("You would enjoy cream cheese with sliced cucumber on your bagel")
+
+elif number == 69:
+    print("we1l... thats a...")
+    time.sleep(1)
+    print("FUNNY NUMBER AHAHA")
+    time.sleep(1)
+    print("You would enjoy NOTHING on your bagel. eat a doughnut instead")
+
+elif number > 69 and number < 100:
+    print("we1l... thats a big number")
+    time.sleep(1)
+    print("let me think now...")
+    time.sleep(1)
+    print("You would enjoy hummus with sliced tomatoes. on your bagel")
+
+else:
+    print("we1l... idk... eat aa doughnut")
+
+input()
